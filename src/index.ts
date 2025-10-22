d50ea24aa05513b0a7a7236442f74c910d9ba436
swimly <979741120@qq.com>
2025-10-22 21:12:07 +0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * @ldesign/tree - 高级树形组件
+ */
+export class VirtualTree {
+  constructor(private container: HTMLElement) { }
+  render(data: any[]) { console.info('Tree rendering:', data.length, 'nodes') }
+}
+export function createTree(container: HTMLElement) { return new VirtualTree(container) }
+
